c71ab1ff24e2ab29a7062f97c3680f913286dc92
HoangDuong-cntt <c2hoangduong@gmail.com>
2026-04-20 08:30:40 +0700

    LẦN THỨ 6

--- thigk2.DangHoangDuong/app/src/main/java/thigk2/DangHoangDuong/thigk/MainActivity.java
+++ thigk2.DangHoangDuong/app/src/main/java/thigk2/DangHoangDuong/thigk/MainActivity.java
@@ -10,8 +10,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void openCN1(View v) { startActivity(new Intent(this, ChucNang1Activity.class)); }
-    public void openCN2(View v) { startActivity(new Intent(this, ChucNang2Activity.class)); }
-    public void openCN3(View v) { startActivity(new Intent(this, ChucNang3Activity.class)); }
-    public void openCN4(View v) { startActivity(new Intent(this, ChucNang4Activity.class)); }
+    public void openCN1(View v) {
+        startActivity(new Intent(this, ChucNang1Activity.class));
+    }
+    public void openCN2(View v) {
+        startActivity(new Intent(this, ChucNang2Activity.class));
+    }
+    public void openCN3(View v) {
+        startActivity(new Intent(this, ChucNang3Activity.class));
+    }
+    public void openCN4(View v) {
+        startActivity(new Intent(this, ChucNang4Activity.class));
+    }
 }
